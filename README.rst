24ace9bf45b6f3b7937624027eb77900bf6ca276
Serg <sergey.panasenko@gmail.com>
2018-03-19 12:37:31 +0200

    1.0.5

--- README.rst
+++ README.rst
@@ -27,3 +27,41 @@ Requirements
 - django-tinymce==2.7.0
 
 
+************
+Requirements
+************
+- Python 3
+- Django==2.0
+- django-ajax-selects==1.7.0
+- django-imagekit==4.0.2
+- django-tinymce==2.7.0
+
+************
+Installation
+************
+
+pip install django-vegetarian-cookbook
+
+add apps in settings.py
+
+    INSTALLED_APPS = [
+        ...
+        'vegetarian_cookbook',
+        'bootstrap4',
+        'imagekit',
+        'tinymce',
+        'ajax_select',
+    ]
+
+add url in urls.py
+
+    from django.conf.urls import include
+
+    urlpatterns = [
+        path('', include('vegetarian_cookbook.urls')),
+        path('admin/', admin.site.urls),
+    ]
+
+more info see in example app
+
+https://github.com/sergey-panasenko/django-vegetarian-cookbook
--- setup.py
+++ setup.py
@@ -3,18 +3,20 @@ from setuptools import setup, find_packages
 with open('README.rst') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
-
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 setup(
     name='django-vegetarian-cookbook',
-    version='1.0.0',
-    packages=find_packages(),
+    version='1.0.5',
+    packages=find_packages(exclude=['example']),
     author='Sergey Panasenko',
     author_email='sergey.panasenko@gmail.com',
     description='Django application for the publication of vegetarian culinary recipes with the calculation of energy and nutrients.',
     long_description=readme,
-    license=license,
+    include_package_data=True,
+    zip_safe=False,
+    license='AGPL v3',
+    install_requires=requirements,
     url='https://github.com/sergey-panasenko/django-vegetarian-cookbook',
 )
